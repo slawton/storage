68a7b90da3328c83959e690fc95291125b15e813
student <student@student-VirtualBox.(none)>
2012-11-28 04:45:41 -0500

    qwer

---
+++ ex1.c
@@ -0,0 +1,26 @@
+//
+// simple inline assembly example
+// For JOS lab 1 exercise 1
+//
+
+#include <stdio.h>
+
+int
+main(int argc, char **argv)
+{
+	int x = 1;
+	printf("Hello x = %d\n", x);
+
+
+	asm("incl %0" : "=r"(x) : "0"(x));
+
+
+	printf("Hello x = %d after increment\n", x);
+
+	if(x == 2){
+		printf("OK\n");
+	}
+	else{
+		printf("ERROR\n");
+	}
+}
